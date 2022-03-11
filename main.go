4b2f8042988a7226eaa55fe53f6e33683ffb3b87
Kolgatina <vak@kodix.ru>
2022-03-11 13:46:24 +0300

    declare averageNumber function

--- main.go
+++ main.go
@@ -8,3 +8,10 @@ import "go-test/input"
 func testValidity(s string) bool {
 	return input.TestValidity(s)
 }
+
+// averageNumber tells is given string valid or not.
+// Difficulty of the function: O(n)
+// Time to implement the function: 20m
+func averageNumber(s string) float64 {
+	return 0
+}
