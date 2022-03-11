2fd9825e5488b50d360c52e36a6d7ec5a65eee03
Kolgatina <vak@kodix.ru>
2022-03-11 13:20:19 +0300

    declare testValidity function

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+// testValidity tells is given string valid or not.
+// Difficulty of the function: O(n)
+// Time to implement the function: 20m
+func testValidity() bool {
+	return true
+}
